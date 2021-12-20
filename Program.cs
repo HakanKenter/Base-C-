3eff3859c1917fb6c231a4b385e9b99794cec530
HakanKenter <hakan.kt@outlook.fr>
2021-12-20 19:44:39 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Permet d'indiquer qu'il faut utiliser différentes fonctionnalités
+// de l'environement dotnet
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("TESST");
+    }
+}
+
