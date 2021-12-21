e9cd71eadd872aee1381318a6f825d1f4fc521cb
HakanKenter <hakan.kt@outlook.fr>
2021-12-21 08:08:43 +0100

    add bases

--- Program.cs
+++ Program.cs
@@ -3,14 +3,32 @@
 
 // Permet d'indiquer qu'il faut utiliser différentes fonctionnalités
 // de l'environement dotnet
+// namespace (espace de nom)
 using System;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("TESST");
+        Console.Write("Hello World! ");
+        // Pour faire biper le terminale
+
+        Console.Beep();
+        
+        // Permet de renouveler le terminal
+        Console.Clear();
+
+        // Ajouter un titre
+        Console.Title = "Hakan Kenter";
+
+        // Ecrire du texte
+        Console.Write("TESST");
+
+        // Pour un retour a la ligne
+        Console.Write("Bonjour ! Comment aller vous ? \n");
+        
+        // Tabulation
+        Console.Write("\t Bonjour ! Comment aller vous ? \n");
     }
 }
 
